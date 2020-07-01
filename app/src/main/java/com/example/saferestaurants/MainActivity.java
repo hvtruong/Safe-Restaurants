484ef19804bca8e46810b03df340665144552625
Jasleen Phangara <phangarajasleen@gmail.com>
2020-06-30 23:31:11 -0700

    Parsing Data complete
    Created new class called 'DataParser' which contains 2 static methods called 'parseRestaurants' and 'parseInspections'. Once the functions are called in the order listed prior, the data is loaded into the Restaurants class and is ready to use.

--- app/src/main/java/com/example/saferestaurants/MainActivity.java
+++ app/src/main/java/com/example/saferestaurants/MainActivity.java
@@ -4,11 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.saferestaurants.model.Restaurant;
+import com.example.saferestaurants.model.Restaurants;
+
+import java.io.FileNotFoundException;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Parsing Data Files //
+        try {
+            DataParser.parseRestaurants("src/main/java/com/example/saferestaurants/ProjectData/restaurants_itr1.csv");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            DataParser.parseInspections("src/main/java/com/example/saferestaurants/ProjectData/inspectionreports_itr1.csv");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        //                   //
+
+        Restaurants restaurants = Restaurants.getInstance();
+
     }
 }
