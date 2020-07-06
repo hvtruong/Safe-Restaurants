9f11ea5997ef73a72c8affd74351e9d8923c8af0
Viet Truong <hvtruong@sfu.ca>
2020-07-05 21:51:46 -0700

    MainActivity.java, RestaurantDetail.java, activity_restaurant_detail.xml:
    -Bug duplicated restaurants when going back fixed
    -Toolbar back button added

--- app/src/main/java/com/example/saferestaurants/RestaurantDetail.java
+++ app/src/main/java/com/example/saferestaurants/RestaurantDetail.java
@@ -13,6 +13,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -33,10 +35,17 @@ public class RestaurantDetail extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_detail);
-
         Toolbar toolbar = findViewById(R.id.singleRestaurantToolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        /*ImageButton backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            finish();
+            }
+        });*/
 
         extractRestaurant();
 
