ab61d026448587a9d8bc98f3a397057a6e531b67
jaiveer <jaiveer_dhanju@sfu.ca>
2020-07-19 21:00:59 -0700

    added toggle button that should change to list view

--- app/src/main/java/com/example/saferestaurants/MapsActivity.java
+++ app/src/main/java/com/example/saferestaurants/MapsActivity.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.location.Location;
@@ -14,6 +15,8 @@ import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.saferestaurants.model.Restaurant;
@@ -56,6 +59,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         setContentView(R.layout.activity_maps);
         //verify permissions
         getLocationAccess();
+        setUpToggleButton();
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
 //        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
 //                .findFragmentById(R.id.map);
@@ -63,6 +67,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         //if(isRestaurantsEmpty())
         //setData();
+    }
+
+    private void setUpToggleButton() {
+        Button btn = (Button) findViewById(R.id.listToggle);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MapsActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 
 
