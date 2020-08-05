ab02c16427f2c42b67898366f0541f24e2aae89b
Jasleen Phangara <phangarajasleen@gmail.com>
2020-08-04 17:21:36 -0700

    Adding Spanish, Portuguese, Catalan

--- app/src/main/java/com/example/saferestaurants/MapsActivity.java
+++ app/src/main/java/com/example/saferestaurants/MapsActivity.java
@@ -15,11 +15,8 @@ import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -40,7 +37,6 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.gson.Gson;
@@ -57,11 +53,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.lang.reflect.Type;
 import java.nio.channels.FileChannel;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.logging.Filter;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -140,10 +133,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         @Override
         protected void onPreExecute() {
             loadingAlert = new ProgressDialog(MapsActivity.this);
-            loadingAlert.setMessage("Updating data, Please wait..");
+            loadingAlert.setMessage(getString(R.string.updating));
             loadingAlert.setCancelable(false);
             loadingAlert.setButton(
-                    DialogInterface.BUTTON_NEGATIVE, "Cancel", new DialogInterface.OnClickListener() {
+                    DialogInterface.BUTTON_NEGATIVE, getString(R.string.cancelButton), new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
                             RetrieveData.this.cancel(true);
@@ -175,7 +168,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         @Override
         protected void onProgressUpdate(Void... values) {
             loadingAlert.getButton(DialogInterface.BUTTON_NEGATIVE).setEnabled(false);
-            loadingAlert.setMessage("Setting data, Please wait..");
+            loadingAlert.setMessage(getString(R.string.setting));
         }
     }
 
@@ -253,11 +246,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private void showUpdatePopUp(){
 
         AlertDialog.Builder builder = new AlertDialog.Builder(MapsActivity.this);
-        builder.setMessage("Do you want to update your restaurant data?");
-        builder.setTitle("Update Available");
+        builder.setMessage(getString(R.string.updatePrompt));
+        builder.setTitle(getString(R.string.updateTitle));
         builder.setCancelable(false);
 
-        builder.setPositiveButton("Update", new DialogInterface.OnClickListener(){
+        builder.setPositiveButton(getString(R.string.updateButton), new DialogInterface.OnClickListener(){
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 dialog.cancel();
@@ -267,7 +260,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         });
 
-        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+        builder.setNegativeButton(getString(R.string.cancelButton), new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) { dialog.cancel(); }
         });
